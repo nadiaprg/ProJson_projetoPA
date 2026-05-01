e3da39e3f7fefbe35fcd1a1c60de4c813de957b2
nadiaprg <nprga@iscte-iul.pt>
2026-05-01 21:29:17 +0100

    JavaDoc das classe JsonObject e JsonValue

--- ProJson/src/main/kotlin/JsonObject.kt
+++ ProJson/src/main/kotlin/JsonObject.kt
@@ -1,27 +1,58 @@
 package org.example
 
+/**
+ * Representa um mapa que associa uma String a um [JsonValue]
+ *
+ * Premite a manipulação dinâmica de elementos, incluindo adicionar novas propriedade,
+ * remover ou modificar propriedades já existentes e consultar valores
+ *
+ * Extende a classe abstrata [JsonValue]
+ *
+ * @property propriedades é o mapa mutável interno que guarda os valores da classe
+ * @property tipo é a variável que guarda o nome da classe original do objeto
+ */
 class JsonObject(
     private var propriedades: MutableMap<String, JsonValue?>,
     private val tipo: String? = null
 ) : JsonValue() {
 
-    // getter da variavel tipo
+    /**
+     * Retorna o valor da variável tipo
+     *
+     * @return a String correspondente ao valor da variável tipo, sendo que pode ser null
+     */
     fun getType(): String? {
         return tipo
     }
 
-    // getter do mapa propriedades
+    /**
+     * Retorna a variável propriedades
+     *
+     * @return o mapa associado à variável propriedades
+     */
     fun getPropriedades(): MutableMap<String, JsonValue?>{
         return propriedades
     }
 
-    // vai buscar 1 propriedade ao mapa
+    /**
+     * Retorna o valor associado a uma chave
+     *
+     * @param propriedade é a string correspondente à chave do valor a devolver
+     * @return o [JsonValue] correspondente à chave ou null se o elemento for nulo
+     */
     fun getPropriedade(propriedade: String): Any? {
         return propriedades[propriedade]
     }
 
-    // altera o valor de uma propriedade
-    // caso a propriedade nao exista cria e associa o valor
+    /**
+     * Modifica ou cria uma propriedade da variável propriedades
+     *
+     * Se a propriedade já existir, atualiza o valor da mesma
+     * Se a propriedade não exitir, cria a mesma e associa o valor dado
+     *
+     * @param propriedade é a string correspondente à chave
+     * @param valor é o valor a associar à chave
+     */
     fun setProperty(propriedade: String, valor: Any? = null){
         // transforma o valor num JsonValue
         val valorJson = ProJson().toJson(valor)
@@ -29,16 +60,20 @@ class JsonObject(
         propriedades[propriedade] = valorJson
     }
 
-    // adicionar propriedade (não sei se vale apenas)
-    fun addProperty(propriedade: String){
-        propriedades[propriedade] = null
-    }
-
-    // eleminar propriedade
+    /**
+     * Remove uma propriedade e o valor associado à mesma do mapa
+     *
+     * @param propriedade é a string correspondente à chave
+     */
     fun removeProperty(propriedade: String){
         propriedades.remove(propriedade)
     }
 
+    /**
+     * Serializa o mapa para o formato textual do Json padrão
+     *
+     * @return uma [String] formatada representando um [JsonObject]
+     */
     override fun toString(): String {
         if (tipo.isNullOrEmpty())
             return propriedades.toList().joinToString(",\n", "{\n", "\n}") {
--- ProJson/src/main/kotlin/JsonValue.kt
+++ ProJson/src/main/kotlin/JsonValue.kt
@@ -1,5 +1,17 @@
 package org.example
 
+/**
+ * Representa a classe base abstrata para todos os tipos de valores JSON.
+ *
+ * Garante que todos os tipos possíveis de valores JSON
+ * são conhecidos na compilação e restritos a esta hierarquia.
+ */
 sealed class JsonValue {
+
+    /**
+     * Retorna a representação textual deste valor JSON.
+     *
+     * @return Uma [String] contendo o valor formatado de acordo com a sintaxe padrão JSON.
+     */
     abstract override fun toString(): String
 }
