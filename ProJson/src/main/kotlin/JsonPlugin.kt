37f2258f3a35cbc91d5aebe38e34b972c93b2cb6
matildeggloria4 <matiglorri@gmail.com>
2026-05-09 12:41:00 +0100

    comentarios do tipo JavaDoc na interface JsonPlugin

--- ProJson/src/main/kotlin/JsonPlugin.kt
+++ ProJson/src/main/kotlin/JsonPlugin.kt
@@ -1,5 +1,18 @@
 package org.example
 
+/**
+ * Interface que define um mecanismo de plugin para personalização da serialização de objetos
+ * Para ser utilizada, uma implementação desta interface deve ser referenciada através da
+ * anotação [JsonString] na classe que se pretende formatar
+ */
+
 interface JsonPlugin {
+
+    /**
+     * Transforma um objeto numa representação customizada em formato de texto
+     *
+     * @param obj o obejeto instanciado que deve ser trnsformado
+     * @return [String] que representa o valor do objeto formatado 
+     */
     fun transform(obj: Any): String
 }
