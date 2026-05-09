49ec260a900a3c575470be29758b0bffff5b885f
matildeggloria4 <matiglorri@gmail.com>
2026-05-09 12:56:43 +0100

    comentarios do tipo JavaDoc na classe de anotação Reference

--- ProJson/src/main/kotlin/Reference.kt
+++ ProJson/src/main/kotlin/Reference.kt
@@ -1,4 +1,11 @@
 package org.example
 
+/**
+ * Classe de anotação utilizada para indicar que uma propriedade deve ser serializada como uma referência JSON
+ *
+ * Em vez de serializar o objeto completo como um objeto JSON, a propriedade anotada
+ * manda o gerador a utilizar um identificador UUID que aponta para o objeto real
+ */
+
 @Target(AnnotationTarget.PROPERTY)
 annotation class Reference()
