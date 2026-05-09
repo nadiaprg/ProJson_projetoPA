28deebeb4812811248a2a5e75e5767dbb320a017
matildeggloria4 <matiglorri@gmail.com>
2026-05-09 12:47:09 +0100

    comentarios do tipo JavaDoc na classe de anotação JsonString

--- ProJson/src/main/kotlin/JsonString.kt
+++ ProJson/src/main/kotlin/JsonString.kt
@@ -2,5 +2,14 @@ package org.example
 
 import kotlin.reflect.KClass
 
+/**
+ * Anotação utilizada para definir uma serialização customizada de uma classe para uma string no formato JSON
+ *
+ * Quando uma classe tem a anotação [JsonString], a classe ProJson ignora a sua serialização padrão como um objeto JSON
+ * e utiliza o plugin fornecido para transformar a instância numa representação textual
+ *
+ * @property plugin é uma [KClass] do plugin responsável pela formatação
+ */
+
 @Target(AnnotationTarget.CLASS)
 annotation class JsonString(val plugin: KClass<*>)
